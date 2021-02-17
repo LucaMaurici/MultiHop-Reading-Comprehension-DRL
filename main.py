620a342b0fc878c08c1d0548f2656ad4d7dc833a
Luca Maurici <57830854+LucaMaurici@users.noreply.github.com>
2021-02-17 18:53:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ from MultiHopEnvironment import MultiHopEnvironment
 
 if __name__ == '__main__':
     env = MultiHopEnvironment()
-    env.reset()
+    state = env.reset()
+
+    N = 30
+    batch_size = 15
+    n_epochs = 2
+    alpha = 0.0003
+    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
+                    alpha=alpha, n_epochs=n_epochs, 
+                    input_dims=env.observation_space.shape)
+
+    
