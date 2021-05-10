87657381af907c8627972a4e4fa6687bbd67a237
LucaMauriciSapienza <82646649+LucaMauriciSapienza@users.noreply.github.com>
2021-05-10 16:25:01 +0200

    Solved bug in startServerNLP
    Now it remains opened for 2 days

--- startServerNLP.py
+++ startServerNLP.py
@@ -3,6 +3,8 @@ from stanza.server import CoreNLPClient
 
 import stanza
 
+import time
+
 corenlp_dir = './corenlp'
 stanza.install_corenlp(dir=corenlp_dir)
 
@@ -15,7 +17,9 @@ os.environ["CORENLP_HOME"] = "./corenlp"
 # start a CoreNLP client
 #with CoreNLPClient(annotators=['tokenize','ssplit','pos','lemma','ner','parse','coref']) as client:
 with CoreNLPClient(annotators=['tokenize']) as client:
-	
-	# run annotation over input
-	ann = client.annotate('')
-	exit()
+    
+    # run annotation over input
+    ann = client.annotate('')
+    time.sleep(172800) #  2 days
+    ann = client.annotate('')
+    #exit()
