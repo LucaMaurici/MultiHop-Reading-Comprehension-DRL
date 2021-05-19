188ee27f0cd7df316cbf8acdb804055ef24efe75
LucaMauriciSapienza <82646649+LucaMauriciSapienza@users.noreply.github.com>
2021-05-20 01:05:37 +0200

    Upgraded plots

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PPO import Agent
-from utils import plot_learning_curve
+from utils import plot_learning_curve, plot_learning_curve_average
 from MultiHopEnvironment import MultiHopEnvironment
 import dill
 
@@ -13,13 +13,13 @@ if __name__ == '__main__':
         encoder = dill.load(f)
     print('Encoder opened')
 
-    N = 40
+    N = 1
     n_actions = 8
-    batch_size = 10
-    n_epochs = 2
-    alpha = 0.0003
-    n_episodes = 200
-    n_steps = 3
+    batch_size = 30
+    n_epochs = 70
+    alpha = 0.003
+    n_episodes = 1500
+    n_steps = 30
     agent = Agent(batch_size=batch_size, alpha=alpha, n_epochs=n_epochs)
 
     print(agent.actor)
@@ -31,35 +31,37 @@ if __name__ == '__main__':
 
 
     for idx_episodes in range(1, n_episodes+1):
-        print(f"\n\n---EPISODE {idx_episodes} ---")
+        print(f"\n---EPISODE {idx_episodes} ---")
         observationOld = env.reset()
-        print(observationOld)
+        #print(observationOld)
         done = False
         score = 0
         idx_steps = 0
         while not done and idx_steps < n_steps:
+        #while idx_steps < n_steps:
             action, prob, val = agent.choose_action(observationOld)
-            print(f"\n\n---STEP: {idx_steps} ---")
-            print("STAMPA 1")
+            #print(f"---STEP: {idx_steps} ---")
+            #print("STAMPA 1")
             observationNew, reward, done = env.step(action)
             score += reward
             agent.remember(observationOld, action, prob, val, reward, done)
-            print("STAMPA 2")
+            #print("STAMPA 2")
             idx_steps += 1
         if idx_episodes % N == 0:
-            print(f"\n\n---LEARN {idx_episodes} ---")
+            #print(f"---LEARN {idx_episodes} ---")
             agent.learn()
+            agent.save_models()
             learn_iters += 1
-        print("STAMPA 3")
+        #print("STAMPA 3")
         observationOld = observationNew
         score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
-        print("STAMPA 4")
+        avg_score = np.mean(score_history[-30:])
+        #print("STAMPA 4")
         #Per futura parte di ricerca salvare il modello quando l'avg_score è maggiore del best
         #if avg_score > best_score:
             #best_score = avg_score
-        agent.save_models()
-        print("STAMPA 5")
+        
+        #print("STAMPA 5")
         idx_episodes += 1
         idx_globalSteps += idx_steps
 
@@ -67,6 +69,7 @@ if __name__ == '__main__':
                 'time_steps', idx_globalSteps, 'learning_steps', learn_iters)
 
     x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve_average(x, score_history, "temp\\learning_curve_average.jpg")
     plot_learning_curve(x, score_history, "temp\\learning_curve.jpg")
 
 
