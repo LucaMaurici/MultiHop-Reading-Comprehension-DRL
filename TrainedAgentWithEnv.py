6806941e4c32a9d5c9cd3ad7bcec64a0bec72927
ValeLorenti <66870181+ValeLorenti@users.noreply.github.com>
2021-05-31 13:32:07 +0200

    EM Score added

--- TrainedAgentWithEnv.py
+++ TrainedAgentWithEnv.py
@@ -3,41 +3,66 @@ import myPredictor
 from MultiHopEnvironment import MultiHopEnvironment, decode_state
 from PPO import Agent
 import warnings
+import utils
 warnings.filterwarnings("ignore")
 
 env = MultiHopEnvironment()
-observationOld, raw_old_state, answer = env.reset()
 
 n_steps = 30
 agent = Agent(batch_size=1, alpha=0.003, n_epochs=1)
 agent.load_models()
 
-done = False
-score = 0
-idx_steps = 0
+n_samples = 10
 
-while not done and idx_steps < n_steps:
-#while idx_steps < n_steps:
-    action, prob, val = agent.choose_action(observationOld)
-    #print(f"---STEP: {idx_steps} ---")
-    #print("STAMPA 1")
-    observationNew, reward, done, raw_new_state = env.step(action)
-    idx_steps += 1
-    score += reward
+for i in range(n_samples):
+    observationOld, raw_old_state, answer = env.reset()
+    done = False
+    score = 0
+    idx_steps = 0
 
-#print(observationNew)
-print("\n\n------------------------------------------------------------------------")
-print(f"Score: {score}")
-print(f"Done: {done}")
+    while not done and idx_steps < n_steps:
+    #while idx_steps < n_steps:
+        action, prob, val = agent.choose_action(observationOld)
+        #print(f"---STEP: {idx_steps} ---")
+        #print("STAMPA 1")
+        observationNew, reward, done, raw_new_state = env.step(action)
+        idx_steps += 1
+        score += reward
 
-question = raw_new_state[0]
-text_to_read = "".join(raw_new_state[2])
-print(f"\nText to read: {text_to_read}")
+    #print(observationNew)
+    print("\n\n------------------------------------------------------------------------")
+    print(f"Score: {score}")
+    print(f"Done: {done}")
 
-prediction = myPredictor.myPredict(text_to_read, question)
-print(f"\nQuestion: {question}")
-print(f"\nPrediction: {prediction}")
-print(f"Correct answer: {answer}")
-print("------------------------------------------------------------------------\n\n")
+    question = raw_new_state[0]
+    text_to_read = "".join(raw_new_state[2])
+    print(f"\nText to read: {text_to_read}")
+
+    prediction = myPredictor.myPredict(text_to_read, question)
+    print(f"\nQuestion: {question}")
+    print(f"\nPrediction: {prediction}")
+    print(f"Correct answer: {answer}")
+
+    em_score_sample = int(utils.exact_match_score(prediction, answer))
+    print(f"EM Score Sample: {em_score_sample}")
+
+    em_score_tot += em_score_sample
+    print("------------------------------------------------------------------------\n\n")
+
+print(f"EM Score Final: {em_score_tot/n_samples}")
+
+
 
 #print(decode_state(observationNew, env.encoder))
+
+'''
+def exact_match(actual, prediction):
+    correct = 0.
+    total = 0
+    for (index, val) in enumerate(actual):
+        for (i, v) in enumerate(val):
+            if (actual[index][i] == prediction[index][i]):
+                correct += 1
+            total += 1
+    return correct/total
+'''
