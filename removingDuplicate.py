0e5119dcfc7b0e037ca5db802ef144751409be43
ValeLorenti <66870181+ValeLorenti@users.noreply.github.com>
2021-06-09 19:16:09 +0200

    Path and Removing Dupl

--- removingDuplicate.py
+++ removingDuplicate.py
@@ -7,66 +7,46 @@ TRAIN_MODE = False
 
 #--- Train ---
 if TRAIN_MODE:
-    graph_path = paths.graph_path_train
-    dataset_path = paths.dataset_path_train
+    graph_path_output = paths.graph_path_train
 #--- Dev ---
 else:
-    graph_path = paths.graph_path_dev
-    dataset_path = paths.dataset_path_dev
+    graph_path_output = paths.graph_path_dev
 
-'''
-with open(dataset_path, "r") as read_file:
-    dataset = json.load(read_file)
-read_file.close()
+graph_path_0 = "CoreferenceGraphsList_dev2_0.pkl"
 
 try:
-    with open(graph_path, 'rb') as f:
-        graphs_list = pickle.load(f)
+    with open(graph_path_0, 'rb') as f:
+        graphs_list_0 = pickle.load(f)
     f.close()
 except:
-    graphs_list = list()
-'''
+    graphs_list_0 = list()
 
-graph_path = "CoreferenceGraphsList_dev_uniqueAnswerFiltered.pkl"
+
+graph_path_1 = "CoreferenceGraphsList_dev2_1.pkl"
 
 try:
-    with open(graph_path, 'rb') as f:
-        graphs_list = pickle.load(f)
+    with open(graph_path_1, 'rb') as f:
+        graphs_list_1 = pickle.load(f)
     f.close()
 except:
-    graphs_list = list()
-
-
-
-graph_path1 = "CoreferenceGraphsList_dev_uniqueAnswerFiltered1.pkl"
-
-try:
-    with open(graph_path, 'rb') as f:
-        graphs_list1 = pickle.load(f)
-    f.close()
-except:
-    graphs_list1 = list()
-
+    graphs_list_1 = list()
 
 
 def removeDuplicate(graphs_list):
     graphs_set = set()
-    new_graphs_list = []
+    new_graphs_list = list()
     for d in graphs_list:
         t = tuple(d.items())
         if t not in graphs_set:
-            graphs_list.add(t)
-            new__graphs_list.append(d)
+            graphs_set.add(t)
+            new_graphs_list.append(d)
 
-    return new__graphs_list
+    return new_graphs_list
 
 
+graphs_list_tot = graphs_list_0 +  graphs_list_1;
+graphs_list_noduplicates = removeDuplicate(graphs_list_tot)
 
-graphs_list_noduplicates = removeDuplicate(graphs_list)
-graphs_list_noduplicates1 = removeDuplicate(graphs_list1)
-
-graphs_list_noduplicate_tot = graphs_list_noduplicates +  graphs_list_noduplicates1;
-
-with open(graph_path, 'wb') as f:
-        pickle.dump(graphs_list, f)
-    f.close()
+with open(graph_path_output, 'wb') as f:
+    pickle.dump(graphs_list_noduplicates, f)
+f.close()
